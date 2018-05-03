7aeea0491941c981a3b7a3f6a2662bf6a01803ab
sgebhard6 <gebhas6@gmail.com>
2018-05-03 18:38:43 -0500

    added equippable item values to Item

--- Assets/Scripts/Editor/ItemEditor.cs
+++ Assets/Scripts/Editor/ItemEditor.cs
@@ -13,6 +13,8 @@ public class ItemEditor : Editor {
 	SerializedProperty uiIconProp;
 	SerializedProperty modelProp;
 	SerializedProperty equippableProp;
+	SerializedProperty equippableSlotTypeProp;
+	SerializedProperty equippableSlotTypeIndexProp;
 
 	void OnEnable()
 	{
@@ -22,11 +24,19 @@ public class ItemEditor : Editor {
 		uiIconProp = serializedObject.FindProperty("uiIcon");
 		modelProp = serializedObject.FindProperty("model");
 		equippableProp = serializedObject.FindProperty("equippable");
+		equippableSlotTypeProp = serializedObject.FindProperty("equippableSlotType");
+		equippableSlotTypeIndexProp = serializedObject.FindProperty("equippableSlotTypeIndex");
 	}
 
 	public override void OnInspectorGUI()
 	{
 		serializedObject.Update();
+		EditorGUILayout.BeginVertical("box");		
+		DrawDefaultInspector();
+		EditorGUILayout.HelpBox("Add a reference to an EquippableSlotTypes Scriptable Object if you are going to equip this item.", MessageType.Info);
+		EditorGUILayout.Space();
+		
+		Item _item = (Item)target;
 
 		itemNameProp.stringValue = EditorGUILayout.TextField("Item Name", itemNameProp.stringValue);
 		stackSizeProp.intValue = EditorGUILayout.IntField("Stack Size", stackSizeProp.intValue);
@@ -34,7 +44,24 @@ public class ItemEditor : Editor {
 		uiIconProp.objectReferenceValue = EditorGUILayout.ObjectField("UI Icon", uiIconProp.objectReferenceValue, typeof(Sprite), false);
 		modelProp.objectReferenceValue = EditorGUILayout.ObjectField(new GUIContent("Item Model", "Leave blank if this item does not need a model"), modelProp.objectReferenceValue, typeof(GameObject), false);
 
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+
 		equippableProp.boolValue = EditorGUILayout.Toggle(new GUIContent("Equippable", "Check this box if this item can be equipped."), equippableProp.boolValue);
+
+		if(equippableProp.boolValue)
+		{
+			//DrawDefaultInspector();
+			if(_item.equippableSlotTypeSO != null)
+			{
+				equippableSlotTypeIndexProp.intValue = EditorGUILayout.Popup(equippableSlotTypeIndexProp.intValue, _item.equippableSlotTypeSO.equippableSlotTypes);
+				equippableSlotTypeProp.stringValue = _item.equippableSlotTypeSO.equippableSlotTypes[equippableSlotTypeIndexProp.intValue];
+			}
+		}
+
+		EditorGUILayout.Space();
+		EditorGUILayout.EndVertical();
 		
 		serializedObject.ApplyModifiedProperties();
 	}
---
+++ Assets/Scripts/ScriptableObject/EquippableSlotType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Inventory/Equippable Slot Type")]
+public class EquippableSlotType : ScriptableObject {
+
+	public string[] equippableSlotTypes;
+}
